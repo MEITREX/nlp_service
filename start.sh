ec32eef8c2c42986bed3a627b9b6fbf1525c70ad
Philipp Kunz <philipp_kunz@outlook.com>
2025-09-12 17:17:07 +0200

    LLAMA updated to 3.1

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ ollama serve &
 sleep 5
 
 # Pull and create models
-ollama pull llama3:8b-instruct-q4_0
+ollama pull llama3.1:8b-instruct-q4_0
 ollama create llama3-instruct-8b:titles -f /service/models/llama-3-8b-instruct/titles/Modelfile
 ollama create llama3-instruct-8b:titles-small -f /service/models/llama-3-8b-instruct/titles-small/Modelfile
 ollama create llama3-instruct-8b:titles-full-sliding-20k -f /service/models/llama-3-8b-instruct/titles-full-sliding-20k/Modelfile
@@ -13,3 +13,4 @@ ollama create llama3-instruct-8b:titles-full-sliding-20k -f /service/models/llam
 # Change working dir and Run python app
 cd /service/embedding_service
 python3 ./app.py
+
